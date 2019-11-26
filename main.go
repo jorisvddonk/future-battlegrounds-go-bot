6a9a9da52019d8aafc701d7c50f26faab25bbbad
joris <jorisvddonk@gmail.com>
2019-11-26 20:46:26 +0100

    Stream battlegrounds; keep turning towards closest ship.

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"math"
-	"time"
 
 	pb "./futurebattlegrounds"
 	v "github.com/ungerik/go3d/float64/vec2"
@@ -32,9 +32,8 @@ func main() {
 	defer conn.Close()
 	c := pb.NewBattlegroundsClient(conn)
 
-	// Contact the server
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	// Establish a context (TODO: do this properly..)
+	ctx := context.Background()
 
 	// Spawn a ship
 	x, err := c.SpawnShip(ctx, &pb.ShipSpawnRequest{IFF: "go-bot"})
@@ -46,61 +45,71 @@ func main() {
 	uuid = x.UUID
 	log.Printf("spawned ship: %s", uuid)
 
-	r, err := c.GetBattleground(ctx, &pb.Empty{})
+	stream, err := c.StreamBattleground(ctx, &pb.Empty{})
 	if err != nil {
-		log.Fatalf("could not get battleground: %v", err)
+		log.Fatalf("could not stream battleground: %v", err)
 	}
 
-	log.Printf("battleground: %d ships", len(r.Ships))
-
-	var ship *pb.Ship
-	for i := 0; i < len(r.Ships); i++ {
-		if r.Ships[i].UUID == uuid {
-			ship = r.Ships[i]
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			break
 		}
-	}
+		if err != nil {
+			log.Fatalf("receiving battleground err: %v", err)
+		}
 
-	if ship != nil {
-		var rotation v.T
-		var position v.T
-		rotation = v.T{ship.RotationVector.X, ship.RotationVector.Y}
-		position = v.T{ship.Position.X, ship.Position.Y}
+		log.Printf("battleground: %d ships", len(r.Ships))
 
-		log.Printf("ship: %s %f %f %f", ship.Position, (rotation.Angle()*180/math.Pi)+90, ship.Hull, ship.Battery)
-		_ = position
-
-		var closestShip *pb.Ship
+		var ship *pb.Ship
 		for i := 0; i < len(r.Ships); i++ {
-			if r.Ships[i] != ship {
-				if closestShip == nil {
-					closestShip = r.Ships[i]
-				} else {
-					len := (pv(v.T{r.Ships[i].Position.X, r.Ships[i].Position.Y})).Sub(&position).Length()
-					if len < (pv(v.T{closestShip.Position.X, closestShip.Position.Y})).Sub(&position).Length() {
+			if r.Ships[i].UUID == uuid {
+				ship = r.Ships[i]
+			}
+		}
+
+		if ship != nil {
+			var rotation v.T
+			var position v.T
+			rotation = v.T{ship.RotationVector.X, ship.RotationVector.Y}
+			position = v.T{ship.Position.X, ship.Position.Y}
+
+			log.Printf("ship: %s %f %f %f", ship.Position, (rotation.Angle()*180/math.Pi)+90, ship.Hull, ship.Battery)
+			_ = position
+
+			var closestShip *pb.Ship
+			for i := 0; i < len(r.Ships); i++ {
+				if r.Ships[i] != ship {
+					if closestShip == nil {
 						closestShip = r.Ships[i]
+					} else {
+						len := (pv(v.T{r.Ships[i].Position.X, r.Ships[i].Position.Y})).Sub(&position).Length()
+						if len < (pv(v.T{closestShip.Position.X, closestShip.Position.Y})).Sub(&position).Length() {
+							closestShip = r.Ships[i]
+						}
 					}
 				}
 			}
-		}
-		if closestShip != nil {
-			log.Printf("Closest: %s", closestShip.Position)
-			closestShipPosRel := position.Scale(1).Sub(pv(v.T{closestShip.Position.X, closestShip.Position.Y}))
-			var relRot int
-			relRot = 0
-			if v.IsLeftWinding(&rotation, closestShipPosRel) {
-				log.Printf("turn left!")
-				relRot = -1
-			} else {
-				log.Printf("turn right!")
-				relRot = 1
-			}
-			_ = relRot
+			if closestShip != nil {
+				log.Printf("Closest: %s", closestShip.Position)
+				closestShipPosRel := position.Scale(1).Sub(pv(v.T{closestShip.Position.X, closestShip.Position.Y}))
+				var relRot int
+				relRot = 0
+				if v.IsLeftWinding(&rotation, closestShipPosRel) {
+					log.Printf("turn left!")
+					relRot = -1
+				} else {
+					log.Printf("turn right!")
+					relRot = 1
+				}
+				_ = relRot
 
-			x, err := c.SetActionState(ctx, &pb.ShipActionStateRequest{UUID: uuid, Thrust: 0, Rotate: float32(relRot), Shooting: false})
-			if err != nil {
-				log.Fatalf("fail: %v", err)
+				x, err := c.SetActionState(ctx, &pb.ShipActionStateRequest{UUID: uuid, Thrust: 0, Rotate: float32(relRot), Shooting: false})
+				if err != nil {
+					log.Fatalf("fail: %v", err)
+				}
+				log.Printf("Command success? %t", x.OK)
 			}
-			log.Printf("Command success? %t", x.OK)
 		}
 	}
 }
