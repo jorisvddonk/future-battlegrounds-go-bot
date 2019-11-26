1e14a75c7688f394f84f244e37b9c4e421968abb
joris <jorisvddonk@gmail.com>
2019-11-26 19:58:34 +0100

    log ship angle from North

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"math"
 	"time"
 
 	pb "./futurebattlegrounds"
+	v "github.com/ungerik/go3d/float64/vec2"
 	"google.golang.org/grpc"
 )
 
@@ -14,6 +16,9 @@ const (
 )
 
 func main() {
+	// North := v.T{0, -1}
+	// East := v.T{1, 0}
+
 	// Set up a connection to the server.
 	log.Printf("Running...")
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
@@ -46,8 +51,15 @@ func main() {
 	for i := 0; i < len(r.Ships); i++ {
 		if r.Ships[i].UUID == uuid {
 			var ship pb.Ship
+			var rotation v.T
+			var position v.T
+
 			ship = *r.Ships[i]
-			log.Printf("ship[%d]: %s %s %f %f", i, ship.Position, ship.RotationVector, ship.Hull, ship.Battery)
+			rotation = v.T{ship.RotationVector.X, ship.RotationVector.Y}
+			position = v.T{ship.Position.X, ship.Position.Y}
+
+			log.Printf("ship[%d]: %s %f %f %f", i, ship.Position, (rotation.Angle()*180/math.Pi)+90, ship.Hull, ship.Battery)
+			_ = position
 		}
 	}
 }
